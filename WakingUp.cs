a49b5c1163c95f8d192183ac754803f45a8d5e94
ari <antoine.ribordy@gmail.com>
2017-01-08 11:36:28 +0100

    feat: Add wakingUp control script to kick off fading in when game starts

---
+++ WakingUp.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class WakingUp : MonoBehaviour {
+	private ScreenFader screenfader;
+
+	private bool faded = true;
+	// Use this for initialization
+
+	/*void Awake()
+	{
+		screenfader = FindObjectOfType<ScreenFader> ();
+		screenfader.fadeIn = true;
+	}
+	*/
+
+	void Start () {
+		screenfader = FindObjectOfType<ScreenFader> ();
+	}
+
+	public IEnumerator WakeUp()
+	{
+		if (faded)
+		{
+			float elapsedTime = 0.0f;
+			while (elapsedTime < 0.01f)
+			{
+				yield return new WaitForEndOfFrame();
+				elapsedTime += Time.deltaTime;
+			}
+		}
+		screenfader.fadeIn = true;
+		faded = false;
+	}
+
+	void Update (){
+		if (!screenfader.fadeIn) {
+			StartCoroutine (WakeUp ());
+		}
+	}
+
+
+
+}
