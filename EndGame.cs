57d2aa736da5fa2fbd94d065fb3574bcbf4afe9d
ari <antoine.ribordy@gmail.com>
2017-03-01 20:12:51 +0100

    feat: Add endgame script playing music and calling endgame scene

---
+++ EndGame.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndGame : MonoBehaviour {
+	public float waitTime = 10.0f;
+	public GameObject canvas;
+
+	private ScreenFader screenfader;
+	private Inventory inventory;
+	private bool faded = false;
+
+
+	// Use this for initialization
+	void Start () {
+		screenfader = FindObjectOfType<ScreenFader> ();
+		inventory = GameObject.FindObjectOfType<Inventory>();
+	}
+
+
+	public IEnumerator FadeOut()
+	//private void FadeOut()
+	{
+		
+		float elapsedTime = 0.0f;
+		while (elapsedTime < 0.01f)
+		{
+			yield return new WaitForEndOfFrame();
+			elapsedTime += Time.deltaTime;
+		}
+		screenfader.fadeIn = false;
+		faded = true;
+	}
+
+	public IEnumerator CallEndGameGUI()
+	{
+		yield return new WaitForSeconds (waitTime);
+		FindObjectOfType<LevelManager> ().LoadLevel ("EndGame");
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void EndTheGame (){
+		// Fade to black at the end of the game
+		//FadeOut();
+		GameObject reticle = GameObject.Find("GvrReticle");
+		reticle.SetActive (false);
+		StartCoroutine (FadeOut ());
+		// Play endgame clip
+		MusicManager musicManager = FindObjectOfType<MusicManager>();
+		AudioSource audio = musicManager.GetComponent<AudioSource>();
+		audio.Play ();
+		// Wait for waitTime seconds, then call endgame scene
+		StartCoroutine (CallEndGameGUI());
+	}
+}
---
+++ MusicManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour
+{
+	static MusicManager instance = null;
+
+	// Use this for initialization
+	void Awake()
+	{
+		if (instance != null) {
+			Destroy (gameObject);
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+			
+	}
+
+	void Update()
+	{
+		
+	}
+
+	void OnLevelWasLoaded(){
+		if (SceneManager.GetActiveScene ().name == "VR world 2") {
+			Debug.Log ("Stop music");
+			AudioSource audio = GetComponent<AudioSource>();
+			audio.Stop ();
+		}
+	}
+
+		
+}
