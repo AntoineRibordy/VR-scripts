c0f2334a587380d630cda951715a416c304ff7ae
ari <antoine.ribordy@gmail.com>
2017-03-01 20:14:31 +0100

    feat: Add LevelManager to load different scenes and restart game

---
+++ LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Application.LoadLevel (name);
+	}
+
+	public void QuitRequest(){
+		Application.Quit ();
+	}
+
+	public void LoadNextLevel(){
+		Application.LoadLevel (Application.loadedLevel + 1);
+	}
+
+}
