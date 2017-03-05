6d4925134e43ef6132a21506d314d4f02e29a65c
ari <antoine.ribordy@gmail.com>
2017-03-05 19:26:42 +0100

    fix: Remove unnecessary code in Interact

--- Interact.cs
+++ Interact.cs
@@ -6,20 +6,18 @@ using System.Collections.Generic;
 [RequireComponent(typeof(Collider))]
 public class Interact: MonoBehaviour, IGvrGazeResponder {
 	public GameObject player;
-	//public GameObject finalObjectToInstantiate;
 	public int numberOfObjectsToBeAdded = 1;
+	public Color color = new Color(0.2f, 0.3f, 0.5f, 1.0f);
 
 	private Vector3 startingPosition;
-	private bool objectPulledOnce = false;
-	private bool objectComplete = false;
 	private Inventory inventory;
 	private ValidateObject validateObject;
 	GameObject cubeFinal;
-	private float fracJourney = 0.5f;
 	private bool itemFromInventory = false;
 	//private bool objectsToBeMerged = false;
 	private GameObject item = null;
 	private int objectsFromInventoryCount = 0;
+	private Color objectColor;
 
 
 
@@ -29,6 +27,7 @@ public class Interact: MonoBehaviour, IGvrGazeResponder {
 		SetGazedAt(false);
 		inventory = GameObject.FindObjectOfType<Inventory>();
 		validateObject = GameObject.FindObjectOfType<ValidateObject> ();
+		objectColor = GetComponent<Renderer> ().material.color;
 	}
 
 	void Update()
@@ -37,13 +36,6 @@ public class Interact: MonoBehaviour, IGvrGazeResponder {
 			InteractFromInventory ();
 			itemFromInventory = false;
 		}
-		/* if (objectComplete)
-		{
-			PlayAudio();
-			GameWon = true;
-		}*/
-			
-
 	}
 
 	void LateUpdate() {
@@ -51,7 +43,7 @@ public class Interact: MonoBehaviour, IGvrGazeResponder {
 	}
 
 	public void SetGazedAt(bool gazedAt) {
-		GetComponent<Renderer>().material.color = gazedAt ? Color.green : Color.red;
+		GetComponent<Renderer>().material.color = gazedAt ? color : objectColor;
 	}
 
 	public void Reset() {
@@ -83,10 +75,7 @@ public class Interact: MonoBehaviour, IGvrGazeResponder {
 
 	void InteractFromInventory ()
 	{
-		//Move current object to the left
-		//transform.position += new Vector3(-0.25f,0,0);
 		//Pull object from inventory to interact
-		Debug.Log("Inventory.Count: " + inventory.inventory.Count);
 		for (int i = 0; i < inventory.inventory.Count; i++){
 			// Check if object is a valid object to interact with
 			if (inventory.inventory [i]) {
@@ -96,79 +85,16 @@ public class Interact: MonoBehaviour, IGvrGazeResponder {
 				}
 			}
 		}
-		Debug.Log ("# items from inventory: " + objectsFromInventoryCount);
 		// If player has all the elements, instantiate final object and destroy current one
 		if(objectsFromInventoryCount >= numberOfObjectsToBeAdded){
 			item = validateObject.InstantiateFinalObject(this.gameObject) as GameObject;
-			Debug.Log("Item: " + item);
 			item.transform.position = transform.position;
 			item.SetActive (true);
-			Destroy (gameObject);
 			inventory.reticleOnObject = false;
-			/*for (int i = 0; i < inventory.inventory.Count; i++) {
-				// Check if object is a valid object to interact with
-				if (inventory.inventory [i]) {
-					if (validateObject.IsObjectValidForInteraction (this.gameObject, inventory.inventory [i])) {
-						inventory.inventory.Remove (inventory.inventory [i]);
-						// Removing items for inventory afterwards creates issues when trying to access gameobjects later
-					}
-				}
-			}
-			*/
-			//ObjectInteract (item);
+			Destroy (gameObject);
 		}
 		objectsFromInventoryCount = 0;
-		/*else {
-			objectsFromInventoryCount = 0;
-		}
-		//Debug.Log ("item: " + item);
-		//Position object to the right & make it active
-		//item.transform.position = Vector3.Lerp (item.transform.position, player.transform.position, fracJourney) + new Vector3 (0.25f, 0.75f, 0) * i;
 
-		// Disable object interaction
-		EventTrigger objectTrigger = item.GetComponent<EventTrigger> ();
-		objectTrigger.enabled = false;
-		objectsFromInventoryCount++;
-
-			
-		// If all components have been added on object, make it inactive
-		if (objectsFromInventoryCount == numberOfObjectsToBeAdded) {
-			EventTrigger trigger = GetComponent<EventTrigger> ();
-			trigger.enabled = false;
-			objectComplete = true;
-		}
-		*/
-
-	}
-
-	void ObjectInteract (GameObject obj)
-	{
-		//Move object from inventory towards current object (will need to put that in an update loop with a timer so the movement is progressive)
-		//item.transform.position = Vector3.Lerp (item.transform.position, transform.position, 0.4f);
-		// Play animation
-		Animation anim = obj.GetComponent<Animation>();
-		anim.Play("AntennaFixing"); // Need to generalise an play whatever animation is on the object
-	}
-
-	/*void MergeObjects()
-	{
-		//Merge them: Set objects as inactive & instantiate new object
-		item.SetActive(false);
-		//gameObject.SetActive(false);
-		//GameObject newitem = Instantiate (inventory.inventory [1]) as GameObject;
-		GameObject newitem = Instantiate (GameObject.FindGameObjectWithTag("CubeFinal")) as GameObject;
-		newitem.SetActive (true);
-		newitem.transform.position = Vector3.Lerp (newitem.transform.position, player.transform.position, fracJourney) + new Vector3 (0.25f, 0.75f, 0);
-	}*/
-
-
-
-	public void PullTowardsPlayer() {
-		//Get player position (GvrViewer)
-		//Add 0.95 * player position to transform
-		GvrViewer player = FindObjectOfType<GvrViewer>();
-		float fracJourney = 0.6f;
-		transform.position = Vector3.Lerp(transform.position, player.transform.position, fracJourney) + new Vector3(0,0.75f,0);
 	}
 
 	#region IGvrGazeResponder implementation
@@ -185,21 +111,12 @@ public class Interact: MonoBehaviour, IGvrGazeResponder {
 	public void OnGazeExit() {
 		SetGazedAt(false);
 		Reset ();
-		objectPulledOnce = false;
 		inventory.reticleOnObject = false;
 	}
 
 	/// Called when the viewer's trigger is used, between OnGazeEnter and OnGazeExit.
 	public void OnGazeTrigger() {
-		/*if (!objectPulledOnce) {
-			PullTowardsPlayer ();
-			objectPulledOnce = true;
-		} else {*/
 			itemFromInventory = true;
-			//inventory.reticleOnObject = false;
-			//ObjectInteract ();
-			//gameObject.SetActive (false);
-		//}
 	}
 
 	#endregion
