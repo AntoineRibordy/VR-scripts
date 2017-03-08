ec048d449e6ca1f00582f830e4aa91fc736af608
ari <antoine.ribordy@gmail.com>
2017-03-08 19:00:02 +0100

    feat: Add InteractiveObject script to limit distance at which player can interact with an object

---
+++ InteractiveObject.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractiveObject : MonoBehaviour {
+	public float distance = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
