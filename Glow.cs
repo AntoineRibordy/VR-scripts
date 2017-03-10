41f8da286e6e3f07731229eef19c19b6514deca4
ari <antoine.ribordy@gmail.com>
2017-03-10 19:54:48 +0100

    feat: Add Glow script so object glow and are more easily found

---
+++ Glow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Glow : MonoBehaviour {
+	private Material mat;
+
+	public float someValue = 0.2f;
+	public bool pickedUp = false;
+	// Use this for initialization
+	void Start () {
+		mat = GetComponent<Renderer>().material;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (!pickedUp) {
+			float emission = Mathf.PingPong (Time.time, 1.0f);
+			Color baseColor = Color.white; //Replace this with whatever you want for your base color at emission level '1'
+
+			Color finalColor = baseColor * someValue * Mathf.LinearToGammaSpace (emission);
+
+			mat.SetColor ("_EmissionColor", finalColor);
+		}
+	
+	}
+}
