6b8161774f394d844c6e3e45f7204d80ad80b87b
ari <antoine.ribordy@gmail.com>
2017-03-11 13:12:18 +0100

    feat: Add MusicPicker to play random music clips during the game

---
+++ MusicPicker.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPicker : MonoBehaviour {
+	[Tooltip("The name of the trigger parameter")]
+	public AudioClip[] soundFiles;
+	public AudioClip deathClip;
+	public AudioClip successClip;
+	public AudioClip stressClip;
+	public AudioClip endMusic;
+	public AudioSource soundSource;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (!soundSource.isPlaying) {
+			PlayNextSong();
+		}
+	}
+
+	private void PlayNextSong (){
+		int index = Random.Range (0, 6);
+		soundSource.clip = soundFiles [index];
+		soundSource.Play ();
+	}
+
+	public void PlayClip (AudioClip name){
+		soundSource.clip = name;
+		soundSource.Play ();
+	}
+
+	public void StopClip (){
+		soundSource.Stop ();
+	}
+}
