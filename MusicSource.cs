0ab7f7aadce6290e816981f4f6f6f3e5a6dbe571
ari <antoine.ribordy@gmail.com>
2017-03-24 19:03:27 +0100

    feat: Add MusicSource script to handle MusicSource gameobject over the different scenes (dontdestroyonload & unique MusicSource)

---
+++ MusicSource.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MusicSource : MonoBehaviour
+{
+	static MusicSource instance = null;
+
+	// Use this for initialization
+	void Awake()
+	{
+		if (instance != null) {
+			Destroy (gameObject);
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+			
+	}
+
+	void Update()
+	{
+		
+	}
+
+	void OnLevelWasLoaded(){
+		if (SceneManager.GetActiveScene ().name == "VR world 2") {
+			AudioSource audio = GetComponent<AudioSource>();
+			audio.Stop ();
+		}
+	}
+
+		
+}
