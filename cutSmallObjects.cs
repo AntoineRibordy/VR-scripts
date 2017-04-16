ab3dbdc64cc7d5d33b7033bab7e44616cf3d67c7
ari <antoine.ribordy@gmail.com>
2017-04-16 18:16:40 +0200

    feat: Add culling script to not display small objects from too far to improve performance

---
+++ cutSmallObjects.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cutSmallObjects : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Camera camera = GetComponent<Camera>();
+		float[] distances = new float[32];
+		distances [8] = 100;
+		camera.layerCullDistances = distances;
+	}
+}
